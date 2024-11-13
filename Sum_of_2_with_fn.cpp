514302852f9d00255b2241a235bb3e66ec29f272
Nobita- Avinash Gond <avinashkumargond955@gmail.com>
2024-11-13 13:18:35 +0530

    Add files via upload

---
+++ Sum_of_2_with_fn.cpp
@@ -0,0 +1,30 @@
+// Sum of two no with fn -->
+
+#include<iostream>
+using namespace std;
+
+// int add()                                    // fn declaration
+// {
+//     int a,b,sum=0;   
+//     cout<<"Enter First no: ";
+//     cin>>a;                                          // fn definition
+//     cout<<"Enter Sec no: ";
+//     cin>>b;
+//     sum=a+b;
+//     cout<<"Sum of both no = "<<sum;
+//     return 0;   
+// }
+// int main(){                                      // main()
+//     add();                                         // fn calling 
+//     return 0;
+// }
+
+
+void myfn();                        // fn declaration
+int main(){                           // main()
+    myfn();                         // fn calling
+    return 0;
+}
+void myfn(){
+    cout<<"Avinash kumar gond :)";                  // fn definition
+}
